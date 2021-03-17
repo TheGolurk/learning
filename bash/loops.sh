a94f7373be9e44563e800ef6c039c0335b35015d
Christian Hernandez <chrismatla68@gmail.com>
2021-03-16 20:42:28 -0600

    adding c like and until

--- bash/loops.sh
+++ bash/loops.sh
@@ -5,8 +5,19 @@ for i in $( ls ); do
 done
 
 # C/PERL Like
+for i in `seq 1 10`;
+do
+	echo $i
+done
+
 COUNTER=0
 while [	$COUNTER -lt 10 ]; do
 	echo The counter is $COUNTER
 	let COUNTER=COUNTER+1
+done
+
+COUNTER=20
+until [ $COUNTER -lt 10 ]; do
+	echo COUNTER $COUNTER
+	let COUNTER-=1
 done
