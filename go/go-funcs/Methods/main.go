7048ed3aa56985da662bec715a0036bc5fe7b19b
Christian Hernandez <chrismatla68@gmail.com>
2021-04-11 21:44:54 -0500

    another big bag of changes

--- go/go-funcs/Methods/main.go
+++ go/go-funcs/Methods/main.go
@@ -4,6 +4,14 @@ import (
 	"github.com/TheGolurk/go-functions/simplemath"
 )
 
+type MathExpr = string
+
+const (
+	AddExpr      = MathExpr("add")
+	SubtractExpr = MathExpr("subtract")
+	MultiplyExpr = MathExpr("multiply")
+)
+
 func main() {
 	sv := simplemath.NewSemanticVersion(1, 2, 3)
 	sv.IncrementMajor()
@@ -12,4 +20,22 @@ func main() {
 	func() {
 		println("hello")
 	}()
+
+	addExpr := mathExpression(AddExpr)
+	println(addExpr(2, 3))
+}
+
+func mathExpression(expr MathExpr) func(float64, float64) float64 {
+	switch expr {
+	case AddExpr:
+		return simplemath.Add
+	case SubtractExpr:
+		return simplemath.Subtract
+	case MultiplyExpr:
+		return simplemath.Multiply
+	default:
+		return func(f1, f2 float64) float64 {
+			return 0
+		}
+	}
 }
--- go/go-funcs/Methods/simplemath/expressions.go
+++ go/go-funcs/Methods/simplemath/expressions.go
@@ -1,1 +1,13 @@
 package simplemath
+
+func Add(f1, f2 float64) float64 {
+	return f1 + f2
+}
+
+func Subtract(f1, f2 float64) float64 {
+	return f1 - f2
+}
+
+func Multiply(f1, f2 float64) float64 {
+	return f1 * f2
+}
