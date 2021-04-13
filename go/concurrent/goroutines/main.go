53d7b42b441b621448e1d5d3efe51265ce83790f
Christian Hernandez <chrismatla68@gmail.com>
2021-04-12 21:57:08 -0500

    I __ a word

--- go/concurrent/goroutines/main.go
+++ go/concurrent/goroutines/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -11,6 +12,34 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i := 0; i < 4; i++ {
 		id := rnd.Intn(4) + 1
-		println(id)
+		if b, ok := queryCache(id); ok {
+			fmt.Println("from cache")
+			fmt.Println(b)
+			continue
+		}
+		if b, ok := queryDatabase(id); ok {
+			fmt.Println("from database")
+			fmt.Println(b)
+			continue
+		}
+		fmt.Printf("Book not found with id: '%v'", id)
+		time.Sleep(150 * time.Millisecond)
 	}
+}
+
+func queryCache(id int) (Book, bool) {
+	b, ok := cache[id]
+	return b, ok
+}
+
+func queryDatabase(id int) (Book, bool) {
+	time.Sleep(100 * time.Millisecond)
+	for _, b := range books {
+		if b.ID == id {
+			cache[id] = b
+			return b, true
+		}
+	}
+
+	return Book{}, false
 }
