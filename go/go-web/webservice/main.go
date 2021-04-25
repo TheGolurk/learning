d04f36284fd769922fb331b3fb14620a1440452b
Christian Hernandez <chrismatla68@gmail.com>
2021-04-25 13:47:05 -0500

    /sigh

--- go/go-web/webservice/main.go
+++ go/go-web/webservice/main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json:"product_id"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"price_per_unit"`
+	QuantityOnHand int    `json:"quantity_on_hand"`
+	ProductName    string `json:"product_name"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productJSON := `[
+		{
+			"product_id": 1,
+			"manufacturer": "some",
+			"sku": "p5fsd12123",
+			"upc": "1231312313",
+			"price_per_unit": "12.312",
+			"quantity_on_hand": 123,
+			"product_name": "nameXD"
+		}
+	]`
+	err := json.Unmarshal([]byte(productJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(productsJson)
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
