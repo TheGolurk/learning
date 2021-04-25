30b9c88f49793597904ff4ebadc61538fdac72bb
Christian Hernandez <chrismatla68@gmail.com>
2021-04-25 18:46:47 -0500

    Locating the required gigapixels to render...

--- go/go-web/webservice/main.go
+++ go/go-web/webservice/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/thegolurk/inventoryservice/database"
 	"github.com/thegolurk/inventoryservice/product"
 )
 
 const apiBasePath = "/api"
 
 func main() {
-
+	database.SetUpDatabase()
 	product.SetUpRoutes(apiBasePath)
 
 	http.ListenAndServe(":5000", nil)
