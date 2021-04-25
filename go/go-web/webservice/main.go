59690db99974ab1345f8ca4f99718c69cbf2fe5a
Christian Hernandez <chrismatla68@gmail.com>
2021-04-25 14:41:57 -0500

    I don't give a damn 'bout my reputation

--- go/go-web/webservice/main.go
+++ go/go-web/webservice/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Product struct {
@@ -33,6 +35,52 @@ func init() {
 	err := json.Unmarshal([]byte(productJSON), &productList)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	product, listItemIndex := findProductByID(productID)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+
+	case http.MethodGet:
+		JSON, err := json.Marshal(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(JSON)
+
+	case http.MethodPut:
+		var updated Product
+		err := json.NewDecoder(r.Body).Decode(&updated)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if updated.ProductID != productID {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		product = &updated
+		productList[listItemIndex] = *product
+		w.WriteHeader(http.StatusOK)
+		return
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
@@ -77,7 +125,17 @@ func getNextID() int {
 	return highestID + 1
 }
 
+func findProductByID(productID int) (*Product, int) {
+	for i, product := range productList {
+		if product.ProductID == productID {
+			return &product, i
+		}
+	}
+	return nil, 0
+}
+
 func main() {
 	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 	http.ListenAndServe(":5000", nil)
 }
